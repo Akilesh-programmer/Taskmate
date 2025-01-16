7881d693dce3cda140a789fc40d73f18afd3a143
Akilesh <akileshakileshs1234@gmail.com>
2025-01-17 00:20:33 +0530

    update notes

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,25 +1,33 @@
-export const AddTask = ({tasklist, setTasklist}) => {
+export const AddTask = ({tasklist, setTasklist, task, setTask}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const date = new Date();
-        // date.getTime will get the millisecond at which task was created.
-        // Impossible to match the millisecond for creating the task.
-        const newTask = {
-            id: date.getTime(), 
-            name: e.target.task.value, 
-            time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
-        };
-        setTasklist([...tasklist, newTask]);
-        e.target.task.value = "";
+        if(task.id) {
+            const date = new Date();
+            const updatedTasklist = tasklist.map((todo) => (
+                todo.id === task.id ? {id: task.id, name: e.target.task.value, time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`} : todo
+            ));
+            setTasklist(updatedTasklist);
+        } else {
+            const date = new Date();
+            // date.getTime will get the millisecond at which task was created.
+            // Impossible to match the millisecond for creating the task.
+            const newTask = {
+                id: date.getTime(), 
+                name: e.target.task.value, 
+                time: `${date.toLocaleTimeString()} ${date.toLocaleDateString()}`
+            };
+            setTasklist([...tasklist, newTask]);
+            e.target.task.value = "";
+        }
     }
     
     return (
         <section className="addTask">
             <form onSubmit={handleSubmit}>
-                <input type="text" name="task" autoComplete="off" placeholder="Add Task" maxLength={25}/>
-                <button type="submit">Add</button>
+                <input type="text" name="task" value={task.name} autoComplete="off" placeholder="Add Task" maxLength={25} onChange={e => setTask({...task, name: e.target.value})} />
+                <button type="submit">{task.id ? "UPDATE" : "ADD"}</button>
             </form>
         </section>
     )
